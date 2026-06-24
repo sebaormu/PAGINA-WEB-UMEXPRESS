3bc52c1ce87370637f198f850d4b96429bffb8a7
sebastian <sebaormu@gmail.com>
2026-06-24 00:56:35 -0500

    contactanos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio-component/inicio-component';
 import { NosotrosComponent } from './componentes/nosotros-component/nosotros-component';
+import { Contactanos } from './componentes/contactanos/contactanos';
 
 export const routes: Routes = [
     { path: '', component: InicioComponent},
-    { path: 'sobre-nosotros', component: NosotrosComponent}
+    { path: 'sobre-nosotros', component: NosotrosComponent},
+    { path: 'contactanos', component: Contactanos}
 ];
